041c9db6a974e6a0f39d2b223b6f786aad8886b9
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2025-08-30 18:26:35 +0530

    30-08-25

---
+++ Striver dsa/DP/Stair_case_optimization.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int staircase(int n ,vector<int>&dp_array){
+    if(n==0||n==1)return 1;
+    if(dp_array[n]!=-1)return dp_array[n];
+    return dp_array[n]=staircase(n-1,dp_array)+staircase(n-2,dp_array);
+}
+int main(){
+    int n=3;
+    vector<int>dp_array(n+1,-1);
+    int ans=staircase(n,dp_array);
+    cout<<ans;
+}
